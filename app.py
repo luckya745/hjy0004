df51061a7445380a4f979acb61c900b6d564f7b3
luckya745 <luckya745@gmail.com>
2026-04-02 13:25:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if selected_name not in st.session_state.history_storage:
     )
     
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.5-flash-lite",
         system_instruction=system_instruction
     )
     
